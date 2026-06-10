2150747614ba9728a3ff022e8aa500439065a869
DokPlay <serrggeejjj@gmail.com>
2026-06-10 22:50:20 +0300

    Use short Linked label

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ import {
   roadmapItems,
   services,
   siteLinks
-} from "../../data/modules.js?v=20260610-linkedin-label";
+} from "../../data/modules.js?v=20260610-linked-label";
 import {
   defaultLanguage,
   faqTranslations,
@@ -14,7 +14,7 @@ import {
   russianTimeZones,
   supportedLanguages,
   uiText
-} from "../../data/i18n.js?v=20260610-linkedin-label";
+} from "../../data/i18n.js?v=20260610-linked-label";
 
 const allCatalogItems = [
   ...freeModules,
