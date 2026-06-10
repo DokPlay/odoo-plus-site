93f303dc985fb4e21f57c23327ae455a5de11ab7
DokPlay <serrggeejjj@gmail.com>
2026-06-10 06:11:32 +0300

    Remove remaining placeholder wording

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ import {
   roadmapItems,
   services,
   siteLinks
-} from "../../data/modules.js?v=20260610-pro-details";
+} from "../../data/modules.js?v=20260610-clean-copy";
 import {
   defaultLanguage,
   faqTranslations,
@@ -14,7 +14,7 @@ import {
   russianTimeZones,
   supportedLanguages,
   uiText
-} from "../../data/i18n.js?v=20260610-pro-details";
+} from "../../data/i18n.js?v=20260610-clean-copy";
 
 const allCatalogItems = [
   ...freeModules,
