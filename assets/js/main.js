3855dd57b05ff06e5abfbd3f487d557a8922cf37
DokPlay <serrggeejjj@gmail.com>
2026-06-10 22:45:08 +0300

    Fix LinkedIn profile link

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ import {
   roadmapItems,
   services,
   siteLinks
-} from "../../data/modules.js?v=20260610-contacts";
+} from "../../data/modules.js?v=20260610-linkedin";
 import {
   defaultLanguage,
   faqTranslations,
@@ -14,7 +14,7 @@ import {
   russianTimeZones,
   supportedLanguages,
   uiText
-} from "../../data/i18n.js?v=20260610-contacts";
+} from "../../data/i18n.js?v=20260610-linkedin";
 
 const allCatalogItems = [
   ...freeModules,
--- data/modules.js
+++ data/modules.js
@@ -2,7 +2,7 @@ export const siteLinks = {
   githubFreeModules: "https://github.com/DokPlay/odoo-plus-site",
   boostyProfile: "https://boosty.to/example",
   email: "mailto:serrggeejjj@gmail.com",
-  linkedin: "https://www.linkedin.com/in/сергей-снаткин-85563a3ab",
+  linkedin: "https://www.linkedin.com/in/%D1%81%D0%B5%D1%80%D0%B3%D0%B5%D0%B9-%D1%81%D0%BD%D0%B0%D1%82%D0%BA%D0%B8%D0%BD-85563a3ab/",
   telegram: "https://t.me/example",
   siteUrl: "https://odoocustom.online/"
 };
